74837d94c0357080db22e230cc0cac03eacc2746
Francisco Sanchez <fransafu@gmail.com>
2023-01-15 19:13:39 -0300

    refactor: enable endpoint

--- src/routes/books.js
+++ src/routes/books.js
@@ -1,13 +1,13 @@
 const express = require('express');
 
-const booksController = require('../controllers/booksController');
+const booksController = require('../controllers/books');
 
 const router = express.Router();
 
 router.get('/', booksController.list);
-// router.get('/:id', booksController.findOne);
-// router.post('/', booksController.create);
-// router.put('/', booksController.create);
-// router.delete('/', booksController.create);
+router.get('/:id', booksController.findOne);
+router.post('/', booksController.create);
+router.put('/', booksController.create);
+router.delete('/', booksController.create);
 
 module.exports = { router };
